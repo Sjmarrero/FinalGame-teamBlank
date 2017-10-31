d8e216ce3d5d4d7a53fec4976ced7565429b0768
Sjmarrero <33099245+Sjmarrero@users.noreply.github.com>
2017-10-31 09:19:26 -0700

    Update gameEngine.java

--- gameEngine.java
+++ gameEngine.java
@@ -1,9 +1,12 @@
 package edu.coo.cs.cs141.final_prog_assignment;
+import java.util.Random;
 
 public class gameEngine {
 	private int playerX, playerY;
+	Random rand = new Random();
 	
 	public characters player = new characters(0,9);
+	
 	
 	
 	
